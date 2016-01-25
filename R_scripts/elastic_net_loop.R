ae6ce126c29f4a19b920830df29f53075a10bc42
Julie Winterburn <julie@penfield.kimel.ric>
2016-01-25 14:55:25 -0500

    Made new R_scripts folder and addded scripts and notes.

---
+++ R_scripts/elastic_net_loop.R
@@ -0,0 +1,19 @@
+best.lambda <- rep(NA,21)
+optimal.nr <- rep(NA,21)
+optimal.dev <- rep(NA,21)
+options(warn=1)
+
+set.seed(100)
+alpha <- seq(0.0,1,0.05)
+
+for (i in  1:21){
+              print(paste(i,":","alpha=",alpha[i]))
+              try(cv.model <- cv.glmnet(gm_pca_train$x[,1:n_pcs],dx[train_indices],family="binomial",alpha=alpha[i],nfolds=10,type.measure="deviance"))
+              cv.lambda <- cv.model$lambda
+              dev.array <- as.array(cv.model$cvm)
+              ncoeff.array <- as.array(cv.model$nzero)
+              best.lambda[i] <- cv.model$lambda.1se
+              optimal.nr[i] <-ncoeff.array[cv.lambda==best.lambda[i]]
+              optimal.dev[i] <- dev.array[cv.lambda==best.lambda[i]]
+              }
+options(warn=0)    #Reset warning-handling to default
